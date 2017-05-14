9aea8a754c1a06858e8edadc51048a49f39f29df
=lxs <ll@test.com>
2017-05-14 21:55:02 +0800

    加载框架

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
+header('Content-type:text/html;charset=utf-8');
 
+//开启调试模式
+define("APP_DEBUG",true);
+//后台模块
+define("APP_PATH",'./Admin/');
+//前台模块
+define('APP_PATH','./Home/');
+
+//加载框架
+require "../ThinkPHP/Think.php";
 
