068951affe025bf69752806da4d2909dfaa12aba
=lxs <ll@test.com>
2017-05-27 17:47:36 +0800

    添加角色并分配权限

--- Admin/Controller/RoleController.class.php
+++ Admin/Controller/RoleController.class.php
@@ -6,10 +6,22 @@ use Admin\Controller;
 class RoleController extends AdminController{
     public function add(){
         if(!empty($_POST)){
+            if($_POST['role_name']==''){
+                $this->error('添加角色失败，角色名称不能为空','add',1);
+                return ;
+            }
+            if($_POST['role_auth_ids']==''){
+                $this->error('添加角色失败，权限不能为空','add',1);
+                return ;
+            }            
+            $ids=implode(',',$_POST['role_auth_ids']);
+            $ids=str_replace('-',',',$ids);
+            $_POST['role_auth_ids']=explode(',',$ids);
+            $_POST['role_auth_ids']=array_unique ($_POST['role_auth_ids']);
             if(D('role')->addRole($_POST)){
                 $this->success('添加角色成功,即将跳转到角色列表','showList',3);
             }else{
-                 $this->error('添加角色失败',1);
+                 $this->error('添加角色失败','add',1);
             }
         }else{
             $auth_infos=M('auth')->order('auth_path asc')->select();
--- public/admin/js/base.js
+++ public/admin/js/base.js
@@ -1,5 +1,9 @@
 $(function() {
+    menuShow();
+    selectByTitle();
+});
 
+function menuShow() {
     $('.menu ul li').mouseover(function() {
         $('.menu-item')[($(this).index())].style.display = 'block';
     });
@@ -12,4 +16,14 @@ $(function() {
     $('.menu-item').mouseout(function() {
         $(this).css('display', 'none');
     });
-});
+}
+
+function selectByTitle() {
+    $('.select-auth ul li h4').click(function() {
+        if ($(this).parent().find('input').attr('checked')) {
+            $(this).parent().parent().find('input').removeAttr('checked'); //取消选择当前权限下的所有选项
+        } else {
+            $(this).parent().parent().find('input').attr('checked', 'checked'); //选择当前权限下的所有选项
+        }
+    });
+}
